4dd60d9d9161d4b2feb47c800311548b77f67e57
felipesalbarelli <felipesalbarelli@gmail.com>
2020-10-27 23:01:34 +0000

    algumas configs, identa msg help

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,8 +2,11 @@ const Database = require("@replit/database")
 
 const db = new Database();
 
-export const createPomdoro = async (pomodoro) => {
+
+
+export const addUser = async (user) => {
   try {
+
   } catch (err) {
     console.error(err);
   }
